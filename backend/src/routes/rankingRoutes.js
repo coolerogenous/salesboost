a00a33259a902a1627e50946135d055d9c5b862e
coolerogenous <coolerogenous@gmail.com>
2026-02-14 16:05:01 +0800

    Reset project for version 2

--- backend/src/routes/rankingRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const rankingController = require('../controllers/rankingController');
-const { authMiddleware } = require('../middlewares/auth');
-
-router.get('/', authMiddleware, rankingController.getLeaderboard);
-router.get('/weekly', authMiddleware, rankingController.getWeeklyLeaderboard);
-
-module.exports = router;
